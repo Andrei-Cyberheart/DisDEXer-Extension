8bbfd2bb30c1d96a6eb3c11de586a82e41d49e2d
Andrei Ulanov <ulanov@live.ru>
2023-04-12 13:08:12 +0300

    feat(popup): Add EventListener for "Logs" button

--- js/popup.js
+++ js/popup.js
@@ -5,9 +5,23 @@ let inputSeconds = document.querySelector("#input-seconds")
 let timeDisplay = document.querySelector("#last-time-display")
 let logButton = document.querySelector("#log-button")
 
+var statusTimer = false
+
 //Initialisation Badge Status
 chrome.storage.sync.get(["status", "disconnect", "currentTime"], (result) => {
     if (result.status) {
         chrome.action.setBadgeText({ text: "ON" })
     }
 });
+
+if (logButton) {
+    logButton.addEventListener("click", async (e) => {
+        console.log("Clicked Log button")
+        chrome.storage.sync.get(null, function(items) {
+            let allKeys = Object.keys(items);
+            let allValues = Object.values(items)
+            console.log(items);
+        });
+        console.log(`statusTimer: ${statusTimer}`)
+    });
+}
