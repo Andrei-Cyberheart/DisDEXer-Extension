b501e0e1712d444b2dcdb12ea09243fa4d10fcac
Andrei Ulanov <ulanov@live.ru>
2023-04-14 13:28:38 +0300

    feat(background): Add Listener alarm
    This alarm will notify the end othe timer with a system notification

--- js/background.js
+++ js/background.js
@@ -1,3 +1,19 @@
+chrome.alarms.onAlarm.addListener((alarm) => {
+    console.log('onAlarm', alarm)
+    chrome.notifications.create('dis-dex', {
+        buttons: [
+            {title: 'Спасибо'}
+        ],
+        iconUrl: '/icons/warning128.png',
+        message: 'Соединение отключено.',
+        priority: 0,
+        title: 'Dialer EXpress',
+        type: 'basic'
+    }, (e) => {
+        console.log("Last error:", chrome.runtime.lastError)
+    })
+})
+
 // Badge Toggle
 chrome.storage.onChanged.addListener((changes, namespace) => {
     if (changes?.status) {
