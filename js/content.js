dba0da7ee3c82c7b9a61b10010a3978eb7cf027a
Andrei Ulanov <ulanov@live.ru>
2023-04-15 13:29:58 +0300

    feat(content): Add content.js

---
+++ js/content.js
@@ -0,0 +1,3 @@
+let time = document.querySelector(".text-sm-body-1 .text-caption");
+let buttonConnect = document.querySelector("#app > div.v-application--wrap > div.toolbar.toolbar > header > div > div:nth-child(3) > button");
+let buttonDisconnect = document.querySelector("#app > div.v-application--wrap > div.toolbar.toolbar > header > div > div:nth-child(4) > button");
