0b597780770bd0914f4fe0cf60905e8c845af775
Andrei Ulanov <ulanov@live.ru>
2023-04-15 13:32:41 +0300

    feat(background): Add Observer for DOM element "Timer"
    This observer saving time to storage

--- js/content.js
+++ js/content.js
@@ -1,3 +1,14 @@
 let time = document.querySelector(".text-sm-body-1 .text-caption");
 let buttonConnect = document.querySelector("#app > div.v-application--wrap > div.toolbar.toolbar > header > div > div:nth-child(3) > button");
 let buttonDisconnect = document.querySelector("#app > div.v-application--wrap > div.toolbar.toolbar > header > div > div:nth-child(4) > button");
+
+var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+        chrome.storage.sync.set({ "currentTime": mutation.target.textContent }, function(){
+            //console.log(mutation.target.textContent)
+        });
+    })
+})
+
+var config = {characterData: true, subtree: true};
+observer.observe(time, config)
