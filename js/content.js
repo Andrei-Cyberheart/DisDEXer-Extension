8bb237434695b9e305c43a876d0d097094b05b16
Andrei Ulanov <ulanov@live.ru>
2023-06-04 14:10:06 +0300

    feat(content): Remove Mutation function
    Removed due to the fact that it did not fit into the load quota

--- js/content.js
+++ js/content.js
@@ -2,16 +2,7 @@ let time = document.querySelector(".text-sm-body-1 .text-caption");
 let buttonConnect = document.querySelector("#app > div.v-application--wrap > div.toolbar.toolbar > header > div > div:nth-child(3) > button");
 let buttonDisconnect = document.querySelector("#app > div.v-application--wrap > div.toolbar.toolbar > header > div > div:nth-child(4) > button");
 
-var observer = new MutationObserver(function(mutations) {
-    mutations.forEach(function(mutation) {
-        chrome.storage.sync.set({ "currentTime": mutation.target.textContent }, function(){
-            //console.log(mutation.target.textContent)
-        });
-    })
-})
 
-var config = {characterData: true, subtree: true};
-observer.observe(time, config)
 
 chrome.storage.onChanged.addListener( (changes, namespace) => {
     //Press Disconnect button
