8b8102045bb32a7caad467d4e9ca75ac38459d28
Andrei Ulanov <ulanov@live.ru>
2023-07-26 18:21:51 +0300

    fix(content.js): Fix selector for time

--- js/content.js
+++ js/content.js
@@ -1,4 +1,4 @@
-let time = document.querySelector(".text-sm-body-1 .text-caption");
+let time = document.querySelector(".text-md-body-1 .text-caption");
 let buttonConnect = document.querySelector("#app > div.v-application--wrap > div.toolbar.toolbar > header > div > div:nth-child(3) > button");
 let buttonDisconnect = document.querySelector("#app > div.v-application--wrap > div.toolbar.toolbar > header > div > div:nth-child(4) > button");
 
